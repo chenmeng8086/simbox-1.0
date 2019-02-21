ffa401b024bc23169d80ca53e2fe7b9eeb06b74e
yanqingzhu <yanqingzhu@hikvision.com.cn>
2019-02-21 15:05:08 +0800

    feat: 修改eslint报错

--- panel/components/symbols-library/symbols/config.js
+++ panel/components/symbols-library/symbols/config.js
@@ -48,8 +48,8 @@ function TreeUtil (data, key, parentKey, map) {
     var pos = {}
     var tree = []
     var i = 0
-    while (data.length != 0) {
-      if (data[i][this.parentKey] == 0) {
+    while (data.length !== 0) {
+      if (data[i][this.parentKey] === 0) {
         var _temp = this.copy(data[i])
         tree.push(_temp)
         pos[data[i][this.key]] = [tree.length - 1]
@@ -57,13 +57,13 @@ function TreeUtil (data, key, parentKey, map) {
         i--
       } else {
         var posArr = pos[data[i][this.parentKey]]
-        if (posArr != undefined) {
+        if (posArr !== undefined) {
           var obj = tree[posArr[0]]
           for (var j = 1; j < posArr.length; j++) {
             obj = obj.children[posArr[j]]
           }
-          var _temp = this.copy(data[i])
-          obj.children.push(_temp)
+          var _temp1 = this.copy(data[i])
+          obj.children.push(_temp1)
           pos[data[i][this.key]] = posArr.concat([obj.children.length - 1])
           data.splice(i, 1)
           i--
@@ -82,7 +82,7 @@ function TreeUtil (data, key, parentKey, map) {
     }
     _temp[this.treeKey] = item[this.key]
     for (var _index in item) {
-      if (_index != this.key && _index != this.parentKey) {
+      if (_index !== this.key && _index !== this.parentKey) {
         var _property = item[_index]
         if ((!!this.map) && this.map[_index]) { _temp[this.map[_index]] = _property } else { _temp[_index] = _property }
       }
