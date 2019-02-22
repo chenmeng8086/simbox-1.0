a10d652e6458e237dfdb45f84d4008b6b102d610
yanqingzhu <yanqingzhu@hikvision.com.cn>
2019-02-22 18:48:48 +0800

    feat: 修改之前

--- panel/components/symbols-library/symbols/symbols-container.vue
+++ panel/components/symbols-library/symbols/symbols-container.vue
@@ -3,10 +3,11 @@
     wrap-class="scrollbar__wrap"
     view-class="scrollbar__view"
   >
+  <div>{{JSON.stringify(originData)}}</div>
   <el-tabs v-model="currentTab" @tab-click="handleClick">
     <el-tab-pane
       v-for="item in options"
-      :label="item.label"
+      :label="item.name"
       :name="item.id"
       :key="item.id">
     </el-tab-pane>
@@ -14,16 +15,30 @@
   <div>
     <template v-if="display==='list'">
       <ul>
-        <li v-for="item in currentChildren" :key="item.id" @click="itemClick">
-          {{item.label}}
+        <li v-for="item in currentChildren" :key="item.id">
+          <template v-if="!item['is_leaf']">
+            <span @click="nodeClick(item)">{{item.name}}</span>
+          </template>
+          <template v-else>
+            <span style="color: red">{{item.name}}</span>
+          </template>
         </li>
       </ul>
     </template>
     <template v-else>
       <div class="childrenContainer">
-        <div class="top">hello</div>
+        <div class="top" style="color: green;border:1px solid red;">{{breadcrumbTitle}}</div>
         <div class="bottom">
-          world
+          <ul>
+            <li v-for="item in content" :key="item.id">
+              <template v-if="!item['is_leaf']">
+                <span @click="nodeContentClick(item)">{{item.name}}</span>
+              </template>
+              <template v-else>
+                <span style="color: red">{{item.name}}</span>
+              </template>
+            </li>
+          </ul>
         </div>
       </div>
     </template>
@@ -59,16 +74,11 @@ export default {
       options: [],
       children: [],
       data: symbolsRecords,
-      display: 'list'
+      display: 'list',
+      breadcrumbTitle: '',
+      content: [],
+      originData: []
     }
-  },
-  created () {
-    const symbolsRecords = [{name: '文字/禁用文字/左/W55%', id: '1'},
-      {name: '文字/二级标题/文字/左/W45%', id: '2'},
-      {name: '文字/禁用文字/左/W35%', id: '3'},
-      {name: '文字/禁用文字/左/W65%', id: '4'}]
-    const data = this.process_symbols(symbolsRecords)
-    console.log('data处理过后的', JSON.stringify(data))
   },
   computed: {
     currentChildren () {
@@ -78,24 +88,48 @@ export default {
   },
   watch: {
     records: function () {
-      // this.str = JSON.stringify(this.records)
-      const data = this.process_symbols(this.records)
-      // this.str = JSON.stringify(data)
-      var tree = new TreeUtil(data, 'id', 'parent')
-      var _data = tree.toTree()
-      this.options = _data
+      const parallelData = this.process_symbols(this.records)
+      this.originData = parallelData
+      console.log(this.originData)
+      const tree = new TreeUtil(parallelData, 'id', 'parent_id')
+      const treeData = tree.toTree()
+      this.options = treeData
+      console.log(this.originData)
+      console.log('hello')
     }
   },
   methods: {
-    handleNodeClick (data) {
+    getTitle (selectedItem) {
+      var str = selectedItem.name
+      const data = this.originData
+      console.log(data)
+      const _item = data.find(item => item.id === selectedItem.id) || {}
+      var parentId = _item['parent_id']
+      while (parentId !== 0) {
+        const _selectedItem = data.find(item => item.id === parentId) || {}
+        console.log(JSON.stringify(_selectedItem))
+        str = `${str}/${_selectedItem.name}`
+        parentId = _selectedItem['parent_id'] || 0
+      }
+      return str
     },
     handleClick (tab, event) {
       const { name } = tab
       this.currentTab = name
       this.display = 'list'
     },
-    itemClick () {
+    nodeClick (item) {
       this.display = 'breadcrumb'
+      const selectedItem = this.deepQuery(this.options, item.id)
+      this.content = selectedItem.children
+      this.breadcrumbTitle = this.getTitle(selectedItem)
+    },
+    nodeContentClick (item) {
+      this.display = 'breadcrumb'
+      const selectedItem = this.deepQuery(this.options, item.id)
+      console.log(this.originData, 'hello')
+      this.content = selectedItem.children
+      this.breadcrumbTitle = this.getTitle(selectedItem)
     },
     dragend (item) {
       window.postMessage('onDragSymbol', item)
@@ -103,13 +137,23 @@ export default {
     findItemByName (arr, name) {
       return arr.filter(item => item.name === name)
     },
-    getParentId (names, i, obj, idsArr) {
-      debugger
-      if (i === 0) return 0
-      const parentName = names[i - 1]
-      const parentItem = Object.values(obj).find(item => item.label === parentName) || {}
-      if (parentItem.id) return parentItem.id
-      return idsArr[i - 1]
+    deepQuery (tree, id) {
+      var isGet = false
+      var retNode = null
+      function deepSearch (tree, id) {
+        for (var i = 0; i < tree.length; i++) {
+          if (tree[i].children && tree[i].children.length > 0) {
+            deepSearch(tree[i].children, id)
+          }
+          if (id === tree[i].id || isGet) {
+            isGet || (retNode = tree[i])
+            isGet = true
+            break
+          }
+        }
+      }
+      deepSearch(tree, id)
+      return retNode
     },
     process_symbols (params) {
       var destDict = {}
@@ -133,74 +177,11 @@ export default {
         }
       }
       return Object.values(destDict)
-    },
-    formDataRecordsNew (records) {
-      const _records = records
-      var obj = {}
-      _records.forEach((item, idx) => {
-        const {id, name} = item
-        const _names = name.split('/')
-        const len = _names.length
-        _names.forEach((_item, _idx) => {
-          const idsArr = new Array(len).fill('0').map(item => getUUID())
-          // 判断是否在
-          let parentId = this.getParentId(_names, _idx, obj, idsArr)
-          let label = _names[_idx]
-          let _id = _idx === len - 1 ? id : idsArr[_idx]
-          obj[`${parentId}_${label}`] = {
-            parent: parentId,
-            id: _id,
-            label
-          }
-        })
-      })
-      return Object.values(obj)
-    },
-    formDataRecords (records) {
-      const _records = this.records
-      var arr = []
-      _records.forEach((item, idx) => {
-        const {id, name} = item
-        const _names = name.split('/')
-        const len = _names.length
-        if (idx === 0) {
-          const idsArr = new Array(len).fill('0').map(item => getUUID())
-          for (let i = 0; i < len; i++) {
-            arr.push({
-              parent: i === 0 ? 0 : idsArr[i - 1],
-              id: i === len - 1 ? id : idsArr[i],
-              label: _names[i]
-            })
-          }
-        }
-        // 需要判断是否在arr中
-        for (let i = 0; i < len; i++) {
-          const idsArr = new Array(len).fill('0').map(item => getUUID())
-          // 判断是否存在时
-          const findItemByName = arr.find(item => item.label === _names[i]) || {}
-          // 如果不存在时候
-          if (!findItemByName.id) {
-            // 找出他的父节点
-            const name = _names[i]
-            const pName = _names[i - 1]
-            const findPItemByName = arr.find(item => item.label === pName) || {}
-            arr.push({
-              parent: i === 0 ? 0 : findPItemByName.id,
-              id: i === len - 1 ? id : idsArr[i],
-              label: name
-            })
-          }
-        }
-      })
-      return arr
     }
   }
 }
 </script>
 <style lang="less" scoped>
-  .childrenContainer{
-
-  }
   .container{
     width: 600px;
     text-align: left;
