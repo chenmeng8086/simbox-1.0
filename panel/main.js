570d4bf77bf749cb0021598846b080dc0e1246f5
yanqingzhu <yanqingzhu@hikvision.com.cn>
2019-03-16 10:42:18 +0800

    去掉dolphin_common

--- panel/main.js
+++ panel/main.js
@@ -11,7 +11,7 @@ import './icons'
 Vue.use(HUI)
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
-document.cookie = 'JSESSIONID=ADAD9C18CF5763DA2981E94FC66255D1'
+document.cookie = 'JSESSIONID=FB3D670FB426CB40162B9DD13310F600'
 Vue.mixin({
   methods: {
     // 全局错误处理方法，传入对象则显示msg字段，传入string则直接显示string内容
